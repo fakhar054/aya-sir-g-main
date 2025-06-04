8f1439fcbfc328f25f8e381e8eeebb7d1755220f
fakhar054 <45002007+fakhar054@users.noreply.github.com>
2025-06-04 12:54:38 +0500

    uncomment parms

--- src/app/components/Filter-bar/Filter-bar.jsx
+++ src/app/components/Filter-bar/Filter-bar.jsx
@@ -6,8 +6,9 @@ import { UserContext } from "@/app/userContext";
 import { useSearchParams } from "next/navigation";
 
 export default function Filter_bar() {
-  // const searchParams = useSearchParams();
+  const searchParams = useSearchParams();
   // const selectedCategoryIdFromURL = searchParams.get("categoryId");
+  // console.log("searchParms", selectedCategoryIdFromURL);
 
   const { apiCategory2, setapiCategories2 } = useContext(UserContext);
   const [active, setActive] = useState(false);
