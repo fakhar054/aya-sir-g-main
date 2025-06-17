5b153602c472051d52066cc59034a328e4d4a036
fakhar054 <45002007+fakhar054@users.noreply.github.com>
2025-06-17 15:38:33 +0500

    changing form model

--- src/app/user-profile/page.jsx
+++ src/app/user-profile/page.jsx
@@ -6,8 +6,52 @@ import { FaEdit } from "react-icons/fa";
 import { UserContext } from "../userContext";
 import Modal from "react-bootstrap/Modal";
 import Button from "react-bootstrap/Button";
+import { FaMicrophone } from "react-icons/fa";
+import { MdDelete } from "react-icons/md";
 
 export default function Page() {
+  const [imagePerview, setImagePreview] = useState("/assets/person_img.png");
+
+  const [isRecording, setIsRecording] = useState(false);
+  const [audioURL, setAudioURL] = useState(null);
+  const mediaRecorderRef = useRef(null);
+  const audioChunksRef = useRef([]);
+  const handleStartRecording = async () => {
+    try {
+      const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+      mediaRecorderRef.current = new MediaRecorder(stream);
+
+      mediaRecorderRef.current.ondataavailable = (event) => {
+        if (event.data.size > 0) {
+          audioChunksRef.current.push(event.data);
+        }
+      };
+
+      mediaRecorderRef.current.onstop = () => {
+        const audioBlob = new Blob(audioChunksRef.current, {
+          type: "audio/webm",
+        });
+        const url = URL.createObjectURL(audioBlob);
+        setAudioURL(url);
+        audioChunksRef.current = [];
+      };
+
+      mediaRecorderRef.current.start();
+      setIsRecording(true);
+    } catch (err) {
+      alert("Microphone access denied or not supported.");
+    }
+  };
+
+  const handleStopRecording = () => {
+    mediaRecorderRef.current.stop();
+    setIsRecording(false);
+  };
+  const handleDeleteAudio = () => {
+    setAudioURL(null);
+    setIsRecording(false);
+  };
+
   const [formData, setFormData] = useState({
     first_name: "",
     last_name: "",
@@ -22,13 +66,13 @@ export default function Page() {
     fileInputRef.current.click();
   };
 
-  const handleFileChange = (e) => {
-    const file = e.target.files[0];
-    if (file) {
-      setSelectedFile(file);
-      setImagePreview(URL.createObjectURL(file));
-    }
-  };
+  // const handleFileChange = (e) => {
+  //   const file = e.target.files[0];
+  //   if (file) {
+  //     setSelectedFile(file);
+  //     setImagePreview(URL.createObjectURL(file));
+  //   }
+  // };
 
   const [showForm, setShowForm] = useState(false);
   const { userInfo, userDetails, setUserDetails } = useContext(UserContext);
@@ -94,6 +138,19 @@ export default function Page() {
     }));
   };
 
+  const handleFileChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setImagePreview(reader.result);
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+
+  const handleImageChange = () => {};
+
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log("form submitted");
@@ -129,15 +186,17 @@ export default function Page() {
                   <Modal.Body className="model_body">
                     <form onSubmit={handleSubmit}>
                       <div
+                        className="image_div"
                         onClick={handleImageClick}
                         style={{ cursor: "pointer" }}
                       >
                         <img
-                          src="/assets/person_img.png"
-                          // src={imagePreview}
+                          // src="/assets/person_img.png"
+                          src={imagePerview}
                           alt="Profile"
                           className="w-32 h-32 rounded-full object-cover"
                         />
+                        <FaEdit className="edit_icon" />
                       </div>
                       <input
                         type="file"
@@ -233,6 +292,31 @@ export default function Page() {
 
                       <div className="input_one_row">
                         <div>
+                          <label htmlFor="contact_no">Gender</label>
+                          <select className="input_auth">
+                            <option value="">--Select --</option>
+                            <option value="male">Male</option>
+                            <option value="female">Female</option>
+                            <option value="others">Others</option>
+                          </select>
+                        </div>
+
+                        <div>
+                          <label htmlFor="address">City</label>
+                          <input
+                            type="text"
+                            className="input_auth"
+                            placeholder="Address"
+                            name="address"
+                            id="address"
+                            onChange={handleChange}
+                            value={formData.address}
+                          />
+                        </div>
+                      </div>
+
+                      <div className="input_one_row">
+                        <div>
                           <label htmlFor="cnic">CNIC</label>
                           <input
                             type="text"
@@ -253,6 +337,90 @@ export default function Page() {
                             placeholder="Age"
                             name="age"
                             id="age"
+                            onChange={handleChange}
+                            // value={formData.address}
+                          />
+                        </div>
+                      </div>
+
+                      <div className="input_one_row">
+                        <div>
+                          <label htmlFor="cnic">CNIC Scan Copy</label>
+                          <input
+                            className="input_auth"
+                            type="file"
+                            name="cnicFront"
+                            onChange={handleImageChange}
+                          />
+                        </div>
+
+                        <div>
+                          <label htmlFor="address">Picture</label>
+                          <input
+                            className="input_auth"
+                            id="cameraInput"
+                            type="file"
+                            accept="image/*"
+                            capture="environment"
+                            onChange={handleImageChange}
+                          />
+                        </div>
+                      </div>
+
+                      <div className="input_one_row">
+                        <div>
+                          <label htmlFor="disability">
+                            Interested Locations
+                          </label>
+                          <input
+                            type="text"
+                            className="input_auth"
+                            placeholder="Disability"
+                            name="disability"
+                            id="disability"
+                            onChange={handleChange}
+                            // value={formData.contact_number}
+                          />
+                        </div>
+
+                        <div>
+                          <label htmlFor="experience">
+                            Last Billing Addresss scan copy
+                          </label>
+                          <input
+                            type="number"
+                            className="input_auth"
+                            placeholder="Experience"
+                            name="experience"
+                            id="experience"
+                            onChange={handleChange}
+                            // value={formData.address}
+                          />
+                        </div>
+                      </div>
+
+                      <div className="input_one_row">
+                        <div>
+                          <label htmlFor="disability">Fields</label>
+                          <input
+                            type="text"
+                            className="input_auth"
+                            placeholder="Disability"
+                            name="disability"
+                            id="disability"
+                            onChange={handleChange}
+                            // value={formData.contact_number}
+                          />
+                        </div>
+
+                        <div>
+                          <label htmlFor="experience">Description</label>
+                          <input
+                            type="number"
+                            className="input_auth"
+                            placeholder="Experience"
+                            name="experience"
+                            id="experience"
                             onChange={handleChange}
                             // value={formData.address}
                           />
@@ -287,32 +455,37 @@ export default function Page() {
                         </div>
                       </div>
 
-                      <div className="input_one_row">
-                        {/* <div>
-                          <label htmlFor="image">Image</label>
-                          <input
-                            type="file"
-                            className="input_auth"
-                            placeholder="Image"
-                            name="image"
-                            id="image"
-                            onChange={handleChange}
-                            // value={formData.contact_number}
-                          />
-                        </div> */}
+                      <div className="input_row_full">
+                        <div className="audio_class">
+                          <div>
+                            <FaMicrophone className="audio_icon" />
 
-                        <div>
-                          <label htmlFor="experience">Audio</label>
-                          <input
-                            type="file"
-                            accept="audio/*"
-                            className="input_auth"
-                            placeholder="Experience"
-                            name="audio"
-                            id="audio"
-                            onChange={handleChange}
-                            // value={formData.address}
-                          />
+                            {!isRecording ? (
+                              <button
+                                onClick={handleStartRecording}
+                                className="btn btn-primary"
+                              >
+                                Start Recording
+                              </button>
+                            ) : (
+                              <button
+                                onClick={handleStopRecording}
+                                className="btn btn-secondary"
+                              >
+                                Stop Recording
+                              </button>
+                            )}
+                          </div>
+
+                          {audioURL && (
+                            <div
+                              className="mt-4 listen_audio"
+                              onClick={handleDeleteAudio}
+                            >
+                              <audio controls src={audioURL}></audio>
+                              <MdDelete className="audio_icon" />
+                            </div>
+                          )}
                         </div>
                       </div>
 
